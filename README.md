38f5a517b6184b4f7ccbcd937ccbf908c0d71b0f
bayan-hmaidy98 <bayanhdy98@gmail.com>
2021-10-25 01:08:21 +0300

    finish the lab

---
+++ README.md
@@ -0,0 +1,3 @@
+## Lab11
+In order to run the application, type in the terminal ./gradlew bootrun after ./gradlew build or from the IDE click on run button. You can reach the page on the port 8080
+localhost:8080 and add the wanted route to move between routes.
---
+++ src/test/java/com/example/songr/SongrApplicationTests.java
@@ -0,0 +1,24 @@
+
+
+package com.example.songr;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+
+@SpringBootTest
+class SongrApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Album newAlbum = new Album("Bat Out of Hell", "meat Loaf", 7,2785,"https://upload.wikimedia.org/wikipedia/en/thumb/0/00/Bat_out_of_Hell.jpg/220px-Bat_out_of_Hell.jpg");
+        Assertions.assertEquals("Bat Out of Hell", newAlbum.getTitle());
+        Assertions.assertEquals("meat Loaf", newAlbum.getArtist());
+        Assertions.assertEquals(7, newAlbum.getSongCount());
+        Assertions.assertEquals(2785, newAlbum.getLength());
+        Assertions.assertEquals("https://upload.wikimedia.org/wikipedia/en/thumb/0/00/Bat_out_of_Hell.jpg/220px-Bat_out_of_Hell.jpg", newAlbum.getImageUrl());
+    }
+
+}
